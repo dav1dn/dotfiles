ad688bf37f172b6c7a0ebe3c6e2ade61588619f6
David <d_nguyen@berkeley.edu>
2015-09-28 09:16:20 +0100

    vim setup .sh

---
+++ .vim/bundle/build.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/scrooloose/nerdcommenter
+git clone https://github.com/kchmck/vim-coffee-script
+git clone https://github.com/pangloss/vim-javascript
+git clone https://github.com/Raimondi/delimitMate
+git clone https://github.com/Shougo/neocomplcache.vim
+git clone https://github.com/Yggdroot/indentline
+
